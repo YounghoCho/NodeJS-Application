c5e0fe5b55256b9a6fb25776bfa437e22d407bc0
jaemining <leejihee950430@gmail.com>
2017-07-03 15:59:17 +0900

    task.js

--- routes/task.js
+++ routes/task.js
@@ -28,5 +28,59 @@ var upload = multer({
     })
 });
 
+//수행자입장에서 검색
+router.get('/', function(req, res) {
+    pool.getConnection(function(error, connection) {
+        if (error) {
+            console.log("getConnection Error" + error);
+            res.sendStatus(500);
+        } else {
+            //id 수정해야 함
+            let selectQuery = 'SELECT status FROM helpers WHERE user_idx = 1';
+            connection.query(selectQuery, function(error, rows) {
+                if(error) {
+                    console.log("Connection Error : " + error);
+                    res.sendStatus(500).send({ message: "Connection Error : " + error });
+                    connection.release();
+                } else {
+                    if (rows == "D"){
+                        console.log("this client already asking");
+                        res.status(400).send({ message: 'this client already asking' }); 
+                        connection.release();
+                    } else {
+                        let home_lat = req.query.home_lat;
+                        let home_long = req.query.home_long;
+                        let workplace_lat = req.query.workplace_lat;
+                        let workplace_long = req.query.workplace_long;
+                        console.log("input : " , home_lat, home_long, workplace_lat, workplace_long);
+                        if (!(home_lat && home_long && workplace_lat && workplace_long))
+                            res.status(400).send({ message: 'wrong inout', result: [] });
+                        else {
+                            let lat = (parseFloat(home_lat) + parseFloat(workplace_lat)) / parseFloat(2.0);
+                            let long = (parseFloat(home_long) + parseFloat(workplace_long)) / parseFloat(2.0);
+                            let r = (lat - home_lat)*(lat - home_lat) + (long - home_long)*(long - home_long);
+                            console.log("center : " , lat, long);
+                            //매칭되지 않고 반경안에 있는 task
+                            let selectQuery = 'SELECT t.*, m.phone, (c.rating/c.rated_count) AS star FROM current_tasks t, clients c, members m WHERE matching_time = ? AND (t.home_lat - ?)*(t.home_lat - ?)+(t.home_long - ?)*(t.home_long - ?) <= ? AND t.clients_members_idx = c.user_idx AND c.user_idx = m.user_id';
+                            var value=["0000-00-00 00:00:00",lat,lat,long,long,r];
+                            connection.query(selectQuery, value, function(error, rows) {
+                                if (error) {
+                                    console.log("Connection Error : " + error);
+                                    res.sendStatus(500).send({ message: 'Connection Error' + error, result: [] });
+                                    connection.release();
+                                } else {
+                                    console.log('Success selecting the task list');
+                                    res.status(200).json({message : "Success in selecting the task list", result : rows});
+                                    res.json(rows).send();
+                                    connection.release();
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+        }
+    });
+});
 
 module.exports = router;
