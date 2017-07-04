67b738e064ae8a480b19d855be978ae338f0abab
jaemining <leejihee950430@gmail.com>
2017-07-04 13:43:11 +0900

    POST /task/helper 수정

--- routes/task.js
+++ routes/task.js
@@ -25,25 +25,27 @@ var upload = multer({
             cb(null, Date.now() + '.' + file.originalname.split('.').pop());
         }
     })
-});//수행자입장에서 검색
-router.post('/helper', function(req, res) {
-    pool.getConnection(function(error, connection) {
+});
+
+//수행자입장에서 검색
+router.post('/helper', function (req, res) {
+    pool.getConnection(function (error, connection) {
         if (error) {
             console.log("getConnection Error" + error);
             res.status(500).send({ message: "Connection Error : " + error, result: [] });
         } else {
             //id 수정해야 함
             let selectQuery = 'SELECT status FROM helpers WHERE user_idx = 1';
-            connection.query(selectQuery, function(error, rows) {
-                if(error) {
+            connection.query(selectQuery, function (error, rows) {
+                if (error) {
                     console.log("Connection Error : " + error);
                     res.status(500).send({ message: "Connection Error : " + error, result: [] });
                     connection.release();
                 } else {
                     //이미 수행중인지 체크
-                    if (rows == "D"){
+                    if (rows == "D") {
                         console.log("this helper already help other client");
-                        res.status(405).send({ message: 'this helper already help other client', result: [] }); 
+                        res.status(405).send({ message: 'this helper already help other client', result: [] });
                         connection.release();
                     } else {
                         //수행중이지 않으면
@@ -51,26 +53,44 @@ router.post('/helper', function(req, res) {
                         let home_long = req.body.home_long;
                         let workplace_lat = req.body.workplace_lat;
                         let workplace_long = req.body.workplace_long;
-                        console.log("input : " , home_lat, home_long, workplace_lat, workplace_long);
+                        console.log("input : ", home_lat, home_long, workplace_lat, workplace_long);
                         //올바른 좌표 들어오는지 체크
-                        if (!(home_lat && home_long && workplace_lat && workplace_long))
+                        if (!(home_lat && home_long && workplace_lat && workplace_long)) {
                             res.status(400).send({ message: 'wrong inout', result: [] });
-                        else {
+                            connection.release();
+                        } else {
                             let lat = (parseFloat(home_lat) + parseFloat(workplace_lat)) / parseFloat(2.0);
                             let long = (parseFloat(home_long) + parseFloat(workplace_long)) / parseFloat(2.0);
-                            let r = (lat - home_lat)*(lat - home_lat) + (long - home_long)*(long - home_long);
-                            console.log("center : " , lat, long);
-                            //매칭되지 않고 반경안에 있는 task lis
-                            let selectQuery = 'SELECT t.*, m.phone, (c.rating/c.rated_count) AS star FROM current_tasks t, clients c, members m WHERE matching_time = ? AND (t.home_lat - ?)*(t.home_lat - ?)+(t.home_long - ?)*(t.home_long - ?) <= ? AND t.clients_members_idx = c.user_idx AND c.user_idx = m.user_id';
-                            var value=["0000-00-00 00:00:00",lat,lat,long,long,r];
-                            connection.query(selectQuery, value, function(error, rows) {
+                            //let r = (lat - home_lat)*(lat - home_lat) + (long - home_long)*(long - home_long);
+                            
+                            function getDistanceFromLatLonInKm(lat1, lng1, lat2, lng2) {
+                                function deg2rad(deg) {
+                                    return deg * (Math.PI / 180);
+                                }
+                                var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+                                var dLon = deg2rad(lng2 - lng1);
+                                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                                var d = 6371 * c; // Distance in km
+                                return d * 0.9;
+                            }
+                            var r = getDistanceFromLatLonInKm(workplace_lat, workplace_long, home_lat, home_long);
+
+                            console.log("center : ", lat, long);
+                            console.log("radius : ", r);
+                            //매칭되지 않고 반경안에 있는 task list
+                            //let selectQuery = 'SELECT t.*, m.phone, (c.rating/c.rated_count) AS star FROM current_tasks t, clients c, members m WHERE matching_time = ? AND (t.home_lat - ?)*(t.home_lat - ?)+(t.home_long - ?)*(t.home_long - ?) <= ? AND t.clients_members_idx = c.user_idx AND c.user_idx = m.user_id';
+                            //let selectQuery = 'SELECT * FROM current_tasks';
+                            let selectQuery = 'SELECT t.*, m.phone, (c.rating/c.rated_count) AS star FROM current_tasks t, clients c, members m WHERE matching_time = ? AND (6371*acos(cos(radians(?))*cos(radians(home_lat))*cos(radians(home_long)-radians(?))+sin(radians(?))*sin(radians(home_lat))))<=? ';
+                            var value = ["0000-00-00 00:00:00", lat, long, lat, r];
+                            connection.query(selectQuery, value, function (error, rows) {
                                 if (error) {
                                     console.log("Connection Error : " + error);
                                     res.status(500).send({ message: 'Connection Error' + error, result: [] });
                                     connection.release();
                                 } else {
                                     console.log("Success in selecting the task list");
-                                    res.status(200).json({message : "Success in selecting the task list", result : rows});
+                                    res.status(200).json({ message: "Success in selecting the task list", result: rows });
                                     connection.release();
                                 }
                             });
