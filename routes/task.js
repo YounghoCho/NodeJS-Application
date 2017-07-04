cb1eed858078a2d0b55e6c14cc0658e929f81e75
jaemining <leejihee950430@gmail.com>
2017-07-04 18:10:43 +0900

    진짜 제발 마지막 task/helper수정

--- routes/task.js
+++ routes/task.js
@@ -82,7 +82,7 @@ router.post('/helper', function (req, res) {
                             //let selectQuery = 'SELECT t.*, m.phone, (c.rating/c.rated_count) AS star FROM current_tasks t, clients c, members m WHERE matching_time = ? AND (t.home_lat - ?)*(t.home_lat - ?)+(t.home_long - ?)*(t.home_long - ?) <= ? AND t.clients_members_idx = c.user_idx AND c.user_idx = m.user_id';
                             //let selectQuery = 'SELECT * FROM current_tasks';
                             let selectQuery = 'SELECT t.*, m.phone, (c.rating/c.rated_count) AS star FROM current_tasks t, clients c, members m WHERE matching_time = ? AND (6371*acos(cos(radians(?))*cos(radians(home_lat))*cos(radians(home_long)-radians(?))+sin(radians(?))*sin(radians(home_lat))))<=? ';
-                            var value = ["0000-00-00 00:00:00", lat, long, lat, r];
+                            let value = ["0000-00-00 00:00:00", lat, long, lat, r];
                             connection.query(selectQuery, value, function (error, rows) {
                                 if (error) {
                                     console.log("Connection Error : " + error);
@@ -117,8 +117,8 @@ router.post('/', function(req, res) {
             let selectQuery = 'SELECT status FROM clients WHERE id = 1';
             connection.query(selectQuery, function(error, rows) {
                 if(error) {
-                    console.log("Connection Error : " + error);
-                    res.sendStatus(500).send({ message: "Connection Error : " + error });
+                    console.log("Connection Error0 : " + error);
+                    res.sendStatus(500).send({ message: "Connection Error0 : " + error });
                     connection.release();
                 } else {
                     if (rows == "A"){
@@ -131,7 +131,7 @@ router.post('/', function(req, res) {
                         let workplace_lat = req.body.workplace_lat;     let workplace_long = req.body.workplace_long;
                         let home_lat = req.body.home_lat;               let home_long = req.body.home_long;
                         let workplace_name = req.body.workplace_name;   let home_name = req.body.home_name;
-                        let phone = req.body.phone;                     let star = req.body.star;
+                        //let phone = req.body.phone;                     let star = req.body.star;
 
                         //올바른 인풋이 아닐때
                         if (!(task_type && cost && details && deadline && workplace_lat && workplace_long
@@ -144,8 +144,8 @@ router.post('/', function(req, res) {
                             let value = [task_type, cost, details, deadline, workplace_lat, workplace_long, home_lat, home_long, workplace_name, home_name, phone, star];
                             connection.query(insertQuery, value, function(err, rows) {
                                 if(err) {
-                                    console.log("Connection Error : " + err);
-                                    res.sendStatus(500).send("Connection Error : " + err);
+                                    console.log("Connection Error1 : " + err);
+                                    res.sendStatus(500).send("Connection Error1 : " + err);
                                     connection.release();
                                 } else {
                                     res.status(200).json({ message : "Success in getting user information" });
@@ -154,11 +154,11 @@ router.post('/', function(req, res) {
                             });
 
                             let updateQuery = 'UPDATE clients SET status = ? WHERE user_idx = ?'
-                            var value = ["D", 1];
+                            let value2 = ["D", 1];
                             connection.query(updateQuery, value, function(error, rows) {
                                 if(error) {
-                                    console.log("Connection Error : " + err);
-                                    res.sendStatus(500).send("Connection Error : " + err);
+                                    console.log("Connection Error2 : " + err);
+                                    res.sendStatus(500).send("Connection Error2 : " + err);
                                     connection.release();
                                 }
                                 else {
